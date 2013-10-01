e88ef19fd2dfc9948a47e8e2fa77728f7b62cb10
Aaron B Magil <amagil@camsys.com>
2013-10-01 18:05:10 -0400

    Starting the Rails Project

---
+++ final/restaurant_simulator/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RestaurantSimulator::Application.config.secret_token = '869c81d225156280f649584102e722024ef7ffa52d5477e6f063fd8d940ebbf13641309f2af72f76d5e7009031eeb64735f6fa96fddec44fcc01f457cc707c94'
---
+++ final/restaurant_simulator/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RestaurantSimulator::Application.config.session_store :cookie_store, key: '_restaurant_simulator_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RestaurantSimulator::Application.config.session_store :active_record_store
