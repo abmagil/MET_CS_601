d94888178a6f81fdab111a5ff543ce4c1489786f
Aaron Magil <amagil@camsys.com>
2013-10-08 20:56:56 -0400

    Progress on WH

--- 13_10_08/hw6_1.js
+++ 13_10_08/hw6_1.js
@@ -1,7 +1,125 @@
 function make_chosen() {
-	console.log("make_chosen");
+	setMake();
+	populateMake();
 }
 
 function model_chosen() {
 	console.log("model_chosen");
 }
+
+
+function setMake() {
+	$("#chosen_make").text($("#make").val());
+}
+
+function populateMake() {
+	
+}
+
+
+carAry = {
+	"Ford" : {
+		"Mustang" : {
+			"C" : 1,
+			"M" : 1,
+			"P" : 1,
+			"A" : 1
+		},
+		"F150" : {
+			"C" : 1,
+			"M" : 1,
+			"P" : 1,
+			"A" : 1
+		},
+		"Fusion" : {
+			"C" : 1,
+			"M" : 1,
+			"P" : 1,
+			"A" : 1
+		},
+		"Focus" : {
+			"C" : 1,
+			"M" : 1,
+			"P" : 1,
+			"A" : 1
+		}
+	},
+	"Toyota" : {
+		"Yaris" : {
+			"C" : 1,
+			"M" : 1,
+			"P" : 1,
+			"A" : 1
+		},
+		"Corolla" : {
+			"C" : 1,
+			"M" : 1,
+			"P" : 1,
+			"A" : 1
+		},
+		"Matrix" : {
+			"C" : 1,
+			"M" : 1,
+			"P" : 1,
+			"A" : 1
+		},
+		"Camry" : {
+			"C" : 1,
+			"M" : 1,
+			"P" : 1,
+			"A" : 1
+		}
+	},
+	"Mazda" : {
+		"Mazda3" : {
+			"C" : 1,
+			"M" : 1,
+			"P" : 1,
+			"A" : 1
+		},
+		"Mazda6" : {
+			"C" : 1,
+			"M" : 1,
+			"P" : 1,
+			"A" : 1
+		},
+		"CX-9" : {
+			"C" : 1,
+			"M" : 1,
+			"P" : 1,
+			"A" : 1
+		},
+		"MX-5" : {
+			"C" : 1,
+			"M" : 1,
+			"P" : 1,
+			"A" : 1
+		}
+	},
+	"Hoooooooonda" : {
+		"Civic" : {
+			"C" : 1,
+			"M" : 1,
+			"P" : 1,
+			"A" : 1
+		},
+		"Accord" : {
+			"C" : 1,
+			"M" : 1,
+			"P" : 1,
+			"A" : 1
+		},
+		"CR-V" : {
+			"C" : 1,
+			"M" : 1,
+			"P" : 1,
+			"A" : 1
+		},
+		"Ridgeline" : {
+			"C" : 1,
+			"M" : 1,
+			"P" : 1,
+			"A" : 1
+		}
+	}
+};
