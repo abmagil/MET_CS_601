c706a1f5c7055dbab160ed75b0f2d9be5eeb5d69
Aaron Magil <amagil@camsys.com>
2013-10-21 17:09:13 -0400

    updating schema, but now it seems like models are out of sync?

--- final/restaurant_simulator/db/migrate/20131017143521_create_restaurants.rb
+++ final/restaurant_simulator/db/migrate/20131017143521_create_restaurants.rb
@@ -1,7 +1,8 @@
 class CreateRestaurants < ActiveRecord::Migration
   def change
     create_table :restaurants do |t|
-
+      t.integer :cash_on_hand
+      t.integer :customers_served
       t.timestamps
     end
   end
--- final/restaurant_simulator/db/migrate/20131017143552_create_tables.rb
+++ final/restaurant_simulator/db/migrate/20131017143552_create_tables.rb
@@ -1,7 +1,9 @@
 class CreateTables < ActiveRecord::Migration
   def change
     create_table :tables do |t|
-
+      t.belongs_to :restaurant
+      t.belongs_to :waiter
+      t.integer :capacity
       t.timestamps
     end
   end
