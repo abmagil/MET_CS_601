0658e493674e68c1935b9cacaaa7af126ad81d4f
Aaron Magil <amagil@camsys.com>
2013-10-24 13:33:17 -0400

    HW for 10/22

---
+++ 13_10_22/shopping/cart.php
@@ -0,0 +1,75 @@
+ <!doctype html>
+<html>
+ <head>
+  <title>CS Flowers</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="styles/stylesheet.css" type="text/css" charset="utf-8" />
+  <link rel="stylesheet" type="text/css" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+  <script src="http://codeorigin.jquery.com/jquery-2.0.3.min.js"></script>
+  <script src="http://codeorigin.jquery.com/ui/1.10.3/jquery-ui.min.js"></script>
+  <script>
+    $(function() {
+		
+		$("input[id*=slider]").on("change", function(event) {
+			var itemId = this.id.split("_")[1];
+			$("#qty_" + itemId).val(this.value);
+			
+		});
+		
+		$("button").button();
+		
+		$("#checkoutBtn").on("click", function(){
+			location="checkout.php";
+		})
+    });
+  </script>
+  
+  <style>
+
+  </style>
+ </head>
+ <body>
+   <div id="name">CS Flowers</div>
+
+   <div id="container">
+	
+     <h1><img src="images/cart.png" alt="Cart">Your Basket Contains</h1>
+
+     <div name="Flower Border">
+      &nbsp;
+     </div>
+
+     <table id="items">
+	  <thead>
+		<tr>
+			<th>Remove</th>
+			<th>Item</th>
+			<th>Price</th>
+			<th>Quantity</th>
+		</tr>
+	 </thead>
+	 <tbody>
+		<tr>
+			<td><img src="images/remove.png" alt="Remove"></td>
+			<td><img name="item_23" src="images/beautiful.jpg" alt="beautiful">
+				&quot;Wild Hearts&quot;</td>
+			<td>$120.00</td>
+			<td>
+				<input type="range" id="slider_23" min="0" max="5" step="1" value="1" >
+				<input type="text" id="qty_23" size="2" maxlength="2" value="1" readonly >
+			</td>
+		</tr>
+	  </tbody>
+    </table>
+
+    <div alt="Continue Checkout Bar">
+      <button type="button" id="checkoutBtn">
+	   <img src="images/cart.png" alt="Continue Checkout">
+	    Continue to checkout
+      </button>
+    </div>
+
+   </div>
+  
+ </body>
+</html>
---
+++ 13_10_22/shopping/checkout.php
@@ -0,0 +1,92 @@
+ <!doctype html>
+<html>
+ <head>
+  <title>CS Flowers</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="styles/stylesheet.css" type="text/css" charset="utf-8" />
+  <link rel="stylesheet" type="text/css" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+  <script src="http://codeorigin.jquery.com/jquery-2.0.3.min.js"></script>
+  <script src="http://codeorigin.jquery.com/ui/1.10.3/jquery-ui.min.js"></script>
+  <script>
+    $(function() {
+		
+		$("input[id*=slider]").on("change", function(event) {
+			var itemId = this.id.split("_")[1];
+			$("#qty_" + itemId).val(this.value);
+			
+		});
+		
+		$("button").button();
+		
+		$("#checkoutBtn").on("click", function(){
+			location="checkout.php";
+		})
+    });
+  </script>
+  
+  <style>
+  img[alt="Pay Now"] {
+	    height: 64px;
+	    width: 64px; 
+	    padding-right: 10px;
+	    vertical-align: middle;
+  }
+  img[alt="Your Information"], img[alt="Billing Address"] {
+	    height: 36px;
+	    width: 36px;
+}
+fieldset {
+	 margin-top: 20px;
+	 font-size: 34px;
+}
+  </style>
+ </head>
+ <body>
+   <div id="name">CS Flowers</div>
+
+   <div id="checkout_container">
+	
+     <h1><img src="images/cart.png" alt="Cart">Checkout</h1>
+
+     <img src="images/flower_line.gif">
+
+     <fieldset>
+	   <legend><img src="images/person.jpeg" alt="Your Information">Your Information</legend>
+     <div>
+      <input type="text" maxlength="25" size="25" name="first_name" id="first_name" alt="First Name" placeholder="First Name">
+     </div>
+
+     <div>
+	   <input type="text" maxlength="25" size="25" name="last_name" id="last_name" alt="Last Name" placeholder="Last Name">
+	 </div>
+     </fieldset>
+
+     <fieldset>
+	  <legend><img src="images/home2.png" alt="Billing Address">Billing Address</legend>
+		
+     <div>
+	   <input type="text" maxlength="255" size="50" name="line1" id="line1" alt="Address Line 1" placeholder="Address (line 1)">
+	 </div>
+
+     <div>
+	   <input type="text" maxlength="255" size="50" name="line2" id="line2" alt="Address Line 2" placeholder="Address (line 2)">
+	 </div>
+
+     <div>
+	   <input type="text" maxlength="25" size="25" name="city" id="city" alt="City" placeholder="City">
+	 </div>
+
+	
+
+
+    <div alt="PayDiv">
+      <button type="button" id="ChargeBtn">
+	   <img src="images/online_payments.png" alt="Pay Now">
+	     Pay Now
+      </button>
+    </div>
+
+   </div>
+  
+ </body>
+</html>
