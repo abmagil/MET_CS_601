f86f763ef7970f4c2f8ba44fae90ab56b4f2c446
Aaron Magil <amagil@camsys.com>
2013-10-29 21:47:59 -0400

    In Class Lab/HW

---
+++ 13_10_29/solution/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>MET CS 601 : Metropolitan College : Wizard Registration (Client)</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h1>Later Tater.</h1>
+	</body>
+</html>
