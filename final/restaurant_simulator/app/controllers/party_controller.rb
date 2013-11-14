839dd76d213d69b62820710fd2df003846104c50
Aaron Magil <amagil@camsys.com>
2013-11-13 20:46:54 -0500

    Removing parties controller (superfluous) and partway to getting button_to to return JSON

--- final/restaurant_simulator/app/controllers/party_controller.rb
+++ final/restaurant_simulator/app/controllers/party_controller.rb
@@ -1,10 +1,20 @@
 class PartyController < ApplicationController
 	def create
 		@party = Party.new
+
+		respond_to do |format|
+			format.json { render json: @party.title}
+			#format.html
+		end
 	end
 
 	def new
 		@party = Party.new
+
+		respond_to do |format|
+			format.json { render json: @party.title}
+			#format.html
+		end
 	end
 	
 	def index
