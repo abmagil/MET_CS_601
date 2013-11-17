71606c00361d4b48c311f03d834633df9d7cb803
Aaron Magil <amagil@camsys.com>
2013-11-17 18:09:51 -0500

    Okay, for real this time...

--- final/restaurant_simulator/db/migrate/20131117012521_create_round_tables.rb
+++
@@ -1,8 +0,0 @@
-class CreateRoundTables < ActiveRecord::Migration
-  def change
-    create_table :round_tables do |t|
-      t.integer :radius
-      t.timestamps
-    end
-  end
-end
--- final/restaurant_simulator/db/migrate/20131117012700_create_rect_tables.rb
+++
@@ -1,9 +0,0 @@
-class CreateRectTables < ActiveRecord::Migration
-  def change
-    create_table :rect_tables do |t|
-      t.integer :height
-      t.integer :width
-      t.timestamps
-    end
-  end
-end
